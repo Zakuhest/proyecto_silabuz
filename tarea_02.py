95834e1b2d27e1060c8fc62580675119dd7015ad
JoseQ <jquisper2@gmail.com>
2022-10-31 17:50:13 -0500

    Se agregó una muestra de imagen en el método pokemon por forma

--- tarea_02.py
+++ tarea_02.py
@@ -1,12 +1,15 @@
 import requests
 import json
+import numpy as np
+import cv2
+import urllib
 
 class PokeApi:
     def __init__(self):
         self.__url=f"https://pokeapi.co/api/v2/"
-        print(requests.get(self.__url))
 
     def get_generation(self,gen:str):
+        #Obtener lista de pokemons por generación
         response = requests.get(self.__url+"generation/"+gen)
         data = response.json()
     
@@ -17,6 +20,7 @@ class PokeApi:
         return lista
 
     def get_forms(self, form:str):
+        #Obtener pokemon por forma
         response = requests.get(self.__url+"pokemon-form/"+form)
         data = response.json()
         forms=data['pokemon']['name']
@@ -27,18 +31,31 @@ class PokeApi:
         print(f"Este es el numero de orden de forma del pokemon {form3}")
 
         if form2:
-            return f"La forma de este pokemon es {form2}"
+            print(f"La forma de este pokemon es {form2}")
         else:
-            return"Este pokemon no tiene formas"
+            print("Este pokemon no tiene formas")
 
-#Instanciar objeto
+        image_url= data['sprites']['front_default']
+        resp = urllib.request.urlopen(image_url)
+        image = np.asarray(bytearray(resp.read()), dtype="uint8")
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        cv2.imshow("Image", image)
+        cv2.waitKey(0)
+        return "\nTerminó el proceso"     
+
+
+
+#Instanciar clase
 pokeapi=PokeApi()
 
+#Opciones a elegir entre las 5 alternativas
 option=input("Ingresa una opción del 1 al 5\n>>>>").strip()
 while option in ("") or option not in ("1","2","3","4","5"):
     print("Por favor, ingresa una opción válida")
     option=input("Ingresa una opción del 1 al 5\n>>>>").strip()
 
+
+#Resultado de las opciones
 if option == "1":
     try:
         generacion=input("Ingresa generación del 1 al 8\n>>>> ")
@@ -52,7 +69,7 @@ elif option=="2":
         print(pokeapi.get_forms(form))
     except Exception as a:
         print(f"{a}\nNo se encontró la información")
-        
+
 elif option=="3":
     pass   
 elif option=="4":
