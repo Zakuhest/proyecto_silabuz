d7e5604668e8bc4a87eceb6811c81706c08d3cc4
CristianD <crisdic5@gmail.com>
2022-10-31 20:59:26 -0500

    Añadiendo menu interactivo

--- tarea_01.py
+++ tarea_01.py
@@ -20,3 +20,35 @@ def convert_author_to_list(author: str):
     list_author.append(word)
 
     return list_author
+
+validation = True
+
+while(validation):
+    print(
+    """
+        Opción 1: Leer archivo.
+        Opción 2: Listar libros.
+        Opción 3: Agregar libros.
+        Opción 4: Eliminar libro.
+        Opción 5: Buscar libros.
+        Opción 6: Ordenar libros por título.
+        Opción 7: Editar datos de un libro.
+        Opción 8: Guardar libro.
+        Opción 9: Salir.
+    """)
+
+    option = input("Ingresar el número de opción a realizar: ")
+    while option not in ('1','2','3','4','5','6','7','8', '9'):
+        print(
+    """
+        Opción 1: Leer archivo.
+        Opción 2: Listar libros.
+        Opción 3: Agregar libros.
+        Opción 4: Eliminar libro.
+        Opción 5: Buscar libros.
+        Opción 6: Ordenar libros por título.
+        Opción 7: Editar datos de un libro.
+        Opción 8: Guardar libro.
+        Opción 9: Salir.
+    """)
+        option = input("Opción Incorrecta. Ingresar nuevamente el número de opción a realizar: ")
