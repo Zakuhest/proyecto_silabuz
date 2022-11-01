70915ea6f944d64350492d22ce759d48f298456f
CristianD <crisdic5@gmail.com>
2022-10-31 20:56:29 -0500

    Añadiendo función para convertir cadena a lista

--- tarea_01.py
+++ tarea_01.py
@@ -6,3 +6,17 @@ class Libro:
         self.isbn = isbn
         self.editorial = editorial
         self.author = author
+
+def convert_author_to_list(author: str):
+    word = ""
+    list_author = []
+
+    for i in author:
+        if i == ',':
+            list_author.append(word)
+            word=""
+            continue
+        word += i
+    list_author.append(word)
+
+    return list_author
